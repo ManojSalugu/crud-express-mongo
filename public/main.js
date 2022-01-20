d10c01206071773a8ccf2aa001db765845506234
ManojSalugu <Manojkumar.s@cawstudios.com>
2022-01-20 15:56:21 +0530

    Application using express.js and mongoDB

---
+++ public/main.js
@@ -0,0 +1,34 @@
+/* globals fetch */
+var update = document.getElementById('update')
+var del = document.getElementById('delete')
+
+update.addEventListener('click', function () {
+  fetch('quotes', {
+    method: 'put',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify({
+      'name': 'Manoj',
+      'quote': 'I find your lack of faith disturbing.'
+    })
+  })
+  .then(response => {
+    if (response.ok) return response.json()
+  })
+  .then(function (response) {
+    window.location.reload()
+  })
+})
+
+del.addEventListener('click', function () {
+  fetch('quotes', {
+    method: 'delete',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      'name': 'Manoj'
+    })
+  }).then(function (response) {
+    window.location.reload()
+  })
+})
